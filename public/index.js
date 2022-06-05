af3ddc472fdd41ad91015e484d048165eccddedc
XMoSalahX <01154615199>
2022-06-05 07:47:25 +0200

    Remove public folder

--- public/index.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector("#menu");
-
-let burger = document.querySelector("#burger");
-
-burger.addEventListener("click", function () {
-  menu.classList.toggle("hidden");
-});
